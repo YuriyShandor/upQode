ade43ce7971910b783574122a1287f696fccc285
Yuriy Shandor <yuriyshandor@gmail.com>
2018-02-01 17:38:36 +0200

    Add Header. Mod_03

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+  $('.toggler-icon').on('click', function() {
+    $(this).toggleClass('toggler-icon_close');
+    $('.nav-bar').slideToggle('0.5s');
+  });
+
   $('.header-slider').slick({
    slidesToShow: 1,
    slidesToScroll: 1,
