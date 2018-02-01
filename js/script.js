e0bc48e66e2dcb5d0c4b5ae7a52ce74ee6569a35
Yuriy Shandor <yuriyshandor@gmail.com>
2018-02-01 20:16:22 +0200

    Add Section Products. Mod_01

--- js/script.js
+++ js/script.js
@@ -9,9 +9,32 @@ $(document).ready(function() {
    slidesToScroll: 1,
    nextArrow: $(".header-slider__nav_next"),
    prevArrow: $(".header-slider__nav_prev"),
-   // autoplay: true,
-   // autoplaySpeed: 5000,
+   autoplay: true,
+   autoplaySpeed: 5000,
    dots: true,
    dotsClass: 'slick-dots__header-slider',
+  });
+
+  $(".products-slider").slick({
+    slidesToShow: 3, // default desktop values
+    slidesToScroll: 3,
+    rows: 2,
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 980, // tablet breakpoint
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+            }
+        },
+        {
+            breakpoint: 480, // mobile breakpoint
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }
+    ]
   });
 });
